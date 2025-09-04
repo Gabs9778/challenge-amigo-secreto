b801342a4367ad9410a545b5de56e82846b95a74
Gabs9778 <gabrielamunozm1997@gmail.com>
2025-09-03 23:42:10 -0500

    funcion para actualizar lista en la interfaz

--- app.js
+++ app.js
@@ -18,7 +18,33 @@ if (amigos.includes(nombreAmigo)) {
     return;
 
 }
+//agregar nombre en array de amigos
+    amigos.push(nombreAmigo);
 
+    //limpiar campo de entrada
+    inputAmigo.value = "";
+
+    //Actualiza lista del HTML
+    actualizarLista ();
     
-};
+}
+ 
+    //funcion para actualizar lista en la interfaz
+
+    function actualizarLista() {
+        const listaAmigos = document.getElementById("listaAmigos");
+    
+    //limpia contenido actual de la lista
+    listaAmigos.innerHTML = "";
+
+   //array con ciclo for
+   for (let i = 0 ; i < amigos.length; i ++ ){
+   const li = document.createElement("li");
+    li.textContent = amigos[i];
+    listaAmigos.appendChild(li);
+   }
+
+}
+
+
 
